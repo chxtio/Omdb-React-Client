db5a8c17db5d94450c2b23f2ce328eff7f8a1bc3
chxtio <chxtio@gmail.com>
2021-06-20 03:10:50 -0700

    Add extra movie details

--- starter-app/src/components/Movies.js
+++ starter-app/src/components/Movies.js
@@ -3,53 +3,11 @@ import './Movies.css';
 import { Card } from 'react-bootstrap';
 
 function Movies(props) {
-
-    // let result = [];
-
-    // for (let movie in props.movies) {
-    //     // console.log("for loop:");
-    //     // console.log(props.movies[movie]);
-    //     async function fetchMyAPI() {
-    //         // const searchParam = encodeURIComponent(query); // URI encode search using escape chars (%)
-    //         // const apiUrl = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}&type=movie&r=json`; // tt0206512
-    //         const apiUrl = `https://www.omdbapi.com/?i=${props.movies[movie].imdbID}&apikey=84f9df6&r=json`;
-    //         // console.log(apiUrl);
-
-    //         let response = await fetch(apiUrl); // call API using fetch
-    //         response = await response.json(); // transform into json
-    //         // console.log(response.Runtime);
-    //         // console.log(response);
-    //         result.push(response);
-
-    //     }
-    //     fetchMyAPI();
-    // }
-
-    // console.log("printing result of length: " + result.length);
-    // console.log(result);
-
-
+    
     return (
         <div>
             {props.movies.map(movie => {
                 console.log("debugging Movies.js");
-                // const apiUrl = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=84f9df6&r=json`;
-                // // console.log(apiUrl);
-                // console.log(movie.imdbID);
-
-
-            //     async function fetchMyAPI() {
-            //     // const searchParam = encodeURIComponent(query); // URI encode search using escape chars (%)
-            //     // const apiUrl = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}&type=movie&r=json`; // tt0206512
-            //     const apiUrl = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=84f9df6&r=json`;
-            //     console.log(apiUrl);
-
-            //     let response = await fetch(apiUrl); // call API using fetch
-            //     response = await response.json(); // transform into json
-            //     // console.log(response.Runtime);
-
-            // }
-            // fetchMyAPI();
 
                 return (
                 <Card style={{ width: '18rem' }}>
@@ -57,19 +15,25 @@ function Movies(props) {
                     <Card.Body>
                         <Card.Title>{movie.Title}</Card.Title>
                         <Card.Text>
-                            Type: {movie.Type}
-                        </Card.Text>
-                        <Card.Text>
-                            Director: {movie.Director}
-                        </Card.Text>
-                        <Card.Text>
                             Release Date: {movie.Released}
                         </Card.Text>
                         <Card.Text>
                             Runtime: {movie.Runtime}
                         </Card.Text>
                         <Card.Text>
+                            Rating: {movie.imdbRating} / 10
+                        </Card.Text>
+                        <Card.Text>
+                            Plot: {movie.Plot}
+                        </Card.Text>
+                        <Card.Text>
+                            Director: {movie.Director}
+                        </Card.Text>
+                        <Card.Text>
                             Genre: {movie.Genre}
+                        </Card.Text>
+                        <Card.Text>
+                            Type: {movie.Type}
                         </Card.Text>
 
                     </Card.Body>
